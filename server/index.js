9f4e0c4199d06b660d083f68d2fc25d3e754d28a
falak <falakgala09@gmail.com>
2023-12-01 16:16:58 +0530

    fetch & display clients & apollo cache

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const colors = require('colors');
 require('dotenv').config();
 
@@ -10,6 +11,8 @@ const connectDB = require('./config/db');
 const port = process.env.PORT || 5000;
 
 const app = express();
+
+app.use(cors());
 
 // Connect to MongoDB
 connectDB();
